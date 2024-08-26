98a56a77877778e11a7e14af3344858ee32c9054
Kuchto47 <32129035+Kuchto47@users.noreply.github.com>
2024-08-26 15:05:43 +0200

    add authguard globally

--- openapi-demo-backend/src/app.module.ts
+++ openapi-demo-backend/src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
+import { APP_GUARD } from '@nestjs/core';
+import { AuthGuard } from './auth/AuthGuard';
 
 @Module({
     imports: [CatsModule],
     controllers: [AppController],
-    providers: [AppService],
+    providers: [AppService, { provide: APP_GUARD, useClass: AuthGuard }],
 })
 export class AppModule {}
