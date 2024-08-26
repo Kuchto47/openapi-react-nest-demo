a404e8677c57112ff78cd203fd2a7ff80395f84c
Kuchto47 <32129035+Kuchto47@users.noreply.github.com>
2024-08-26 15:14:43 +0200

    add ability to have public paths

--- openapi-demo-backend/src/auth/AuthGuard.ts
+++ openapi-demo-backend/src/auth/AuthGuard.ts
@@ -4,23 +4,34 @@ import {
     Injectable,
     UnauthorizedException,
 } from '@nestjs/common';
+import { Request } from 'express';
+import { Reflector } from '@nestjs/core';
+import { IS_PUBLIC_KEY } from './PublicRoute';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-    constructor() {}
+    constructor(private reflector: Reflector) {}
 
     async canActivate(context: ExecutionContext): Promise<boolean> {
+        const isPublicRoute = this.reflector.getAllAndOverride<boolean>(
+            IS_PUBLIC_KEY,
+            [context.getHandler(), context.getClass()],
+        );
+
+        if (isPublicRoute) {
+            return true;
+        }
+
         const request = context.switchToHttp().getRequest();
         const token = this.extractTokenFromHeader(request);
-        if (!token || token !== 'EXAMPLE DEMO TOKEN') {
+        if (!token || token !== 'EXAMPLE_DEMO_TOKEN') {
             throw new UnauthorizedException();
         }
         return true;
     }
 
     private extractTokenFromHeader(request: Request): string | undefined {
-        const [type, token] =
-            request.headers.get('Authorization')?.split(' ') ?? [];
+        const [type, token] = request.headers.authorization?.split(' ') ?? [];
         return type === 'Bearer' ? token : undefined;
     }
 }
